5411a66a7ca4626b4c23431c6ca5baa067d13c75
Jim Mangiafico <jim@jurisdatum.com>
2025-04-17 14:37:18 -0400

    error pages

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 from routes.items import items_bp
 from routes.interpretation import interp_bp
@@ -9,6 +9,10 @@ app = Flask(__name__)
 app.register_blueprint(items_bp)
 app.register_blueprint(interp_bp)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('404.html'), 500
