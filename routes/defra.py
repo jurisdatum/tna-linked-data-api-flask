d7712567198e09988a98af6c15e27d2b15b77000
Jim Mangiafico <jim@jurisdatum.com>
2025-05-28 10:14:27 -0400

    review date facets

--- routes/defra.py
+++ routes/defra.py
@@ -24,15 +24,18 @@ def defra_lists():
     filtered_args = { key: request.args[key] for key in request.args if key in allowed_keys }
     data = fetch(**filtered_args)
     enhance_status_counts(data)
-    data['grouped'] = {
-        'byYear': enhance_year_counts(data)
-    }
+    enhance_year_counts(data)
     enhance_type_counts(data)
     enhance_chapter_counts(data)
     enhance_extent_counts(data)
     enhance_source_counts(data)
     enhance_regulator_counts(data)
     enhance_subject_counts(data)
+    enhance_review_counts(data)
+    data['grouped'] = {
+        'byYear': group_year_counts(data),
+        'byReviewDate': group_review_counts(data)
+    }
     data['cancel_links'] = make_cancel_links(data)
     pager = pagination_data(
         current = data['query']['page'],
@@ -44,23 +47,14 @@ def defra_lists():
     return render_template('defra/main.html', data=data, pager=pager)
 
 
+# add links to counts
+
 def enhance_status_counts(data):
-    base = request.base_url + '?'
-    other_params = prune_params(data['query'], 'status')
-    if other_params:
-        base += urlencode(other_params) + '&'
-    for count in  data['counts']['byStatus']:
-        count['link'] = base + 'status=' + count['id']
+    add_links_to_counts(data, 'status', 'byStatus')
 
 
 def enhance_year_counts(data):
-    base = request.base_url + '?'
-    other_params = prune_params(data['query'], 'year')
-    if other_params:
-        base += urlencode(other_params) + '&'
-    for count in  data['counts']['byYear']:
-        count['link'] = base + 'year=' + str(count['year'])
-    return [list(batch) for batch in batched(data['counts']['byYear'], 12)]
+    add_links_to_year_counts(data, 'year', 'byYear')
 
 
 def enhance_type_counts(data):
@@ -87,6 +81,10 @@ def enhance_subject_counts(data):
     add_links_to_counts(data, 'subject', 'bySubject')
 
 
+def enhance_review_counts(data):
+    add_links_to_year_counts(data, 'review', 'byReviewDate')
+
+
 def add_links_to_counts(data, queryParam, countsKey):
     base = request.base_url + '?'
     other_params = prune_params(data['query'], queryParam)
@@ -95,6 +93,26 @@ def add_links_to_counts(data, queryParam, countsKey):
     for count in  data['counts'][countsKey]:
         count['link'] = base + queryParam + '=' + count['id']
 
+
+def add_links_to_year_counts(data, queryParam, countsKey):
+    base = request.base_url + '?'
+    other_params = prune_params(data['query'], queryParam)
+    if other_params:
+        base += urlencode(other_params) + '&'
+    for count in  data['counts'][countsKey]:
+        count['link'] = base + queryParam + '=' + str(count['year'])
+
+
+# group yearly counts
+
+def group_year_counts(data):
+    return [list(batch) for batch in batched(data['counts']['byYear'], 12)]
+
+def group_review_counts(data):
+    return [list(batch) for batch in batched(data['counts']['byReviewDate'], 12)]
+
+
+# make cancel links
 
 def make_cancel_links(data):
     return {
@@ -105,7 +123,8 @@ def make_cancel_links(data):
         'extent': make_cancel_link(data['query'], 'extent'),
         'source': make_cancel_link(data['query'], 'source'),
         'regulator': make_cancel_link(data['query'], 'regulator'),
-        'subject': make_cancel_link(data['query'], 'subject')
+        'subject': make_cancel_link(data['query'], 'subject'),
+        'review': make_cancel_link(data['query'], 'review')
     }
 
 def make_cancel_link(query, key):
